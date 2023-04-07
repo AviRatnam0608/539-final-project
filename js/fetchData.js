8b84756aa34fc541ee01c575950bb8d98d6caf1e
Avinash Ratnam <avinashr@umich.edu>
2023-04-07 04:53:32 -0400

    Finished a good chunk

--- js/fetchData.js
+++ js/fetchData.js
@@ -3,14 +3,16 @@ const END_POINT =
   "https://api.spoonacular.com/food/restaurants/search?cuisine=";
 
 const searchBox = document.querySelector("#inputQuery");
-
+let increment = 0;
 let resultsArray = localStorage.getItem("searched-results")
   ? JSON.parse(localStorage.getItem("searched-results"))
   : NaN;
 
 const fetchData = async (event) => {
   event.preventDefault();
+  increment = 0;
   const searchVal = searchBox.value;
+  document.querySelector("#search-result").innerHTML = `<div>Loading...</div>`;
   const myFetch = await fetch(`${END_POINT}${searchVal}`, {
     method: "GET",
     headers: {
@@ -20,33 +22,49 @@ const fetchData = async (event) => {
   const data = await myFetch.json();
   resultsArray = data;
   localStorage.setItem("searched-results", JSON.stringify(resultsArray));
+  document.querySelector(
+    "#search-result"
+  ).innerHTML = `<a id="search-button" href="resultsPage.html">view all ${resultsArray["restaurants"].length} results</a>
+  `;
   console.log(resultsArray);
 };
 
 const contentSection = document.querySelector("#content-section");
-let increment = 0;
+
 const showData = (n) => {
   console.log(resultsArray);
   resultsArray["restaurants"]
-    .slice(0, (increment = increment + n))
+    .slice(increment, (increment = increment + n))
     .forEach((element, i) => {
-      const cardDiv = `<div class="card">
+      const cardDiv = `<div class="card" key=${i}>
     <div class="card-headers">
       <img
         src=${element.logo_photos[0]}
+        alt=${element.name}
       />
     </div>
     <div class="card-content">
       <h3>${element.name}</h3>
       <div>
-        <h3>${"$".repeat(element.dollar_signs)}</h3>
-        <h4>★ ${element.weighted_rating_value.toFixed(1)}</h4>
+        <h3>${
+          element.dollar_signs ? "$".repeat(element.dollar_signs) : "N/A"
+        }</h3>
+        <h4>★ ${
+          element.weighted_rating_value
+            ? element.weighted_rating_value.toFixed(1)
+            : "N/A"
+        }</h4>
       </div>
     </div>
     <div class="card-footer">
       <p>${element.address.street_addr}</p>
-      <p>${
-        element.pickup_enabled ? "Pickup Available" : "Pickup Not Available"
+      <p class=${
+        element.offers_first_party_delivery ? "available" : "unavailable"
+      }>
+      ${
+        element.offers_first_party_delivery
+          ? "Delivery Available"
+          : "Delivery Not Available"
       }</p>
     </div>
   </div>`;
